46f1a0ea1ae199fbef719c91b3cf589b727e6c89
Ondrej Certik <ondrej@certik.cz>
2009-07-08 16:10:51 -0600

    pcolor exported

--- demo6.py
+++ demo6.py
@@ -2,33 +2,9 @@
 Implements matplotlib's pcolor() like function, that produces a vtk file.
 """
 
-from pylab import meshgrid, arange, cos, sin, pcolor, show
+from pylab import meshgrid, arange, cos, sin
 import visit_writer
-
-def pcolor_unstructured(X, Y, C):
-    """
-    Does "pcolor" plot and returns points, connectivity and zonal & nodal data.
-    """
-    pts = []
-    connectivity = []
-    zonal = []
-    nodal = []
-    w = X.shape[0]
-    h = X.shape[1]
-    assert w == Y.shape[0]
-    assert h == Y.shape[1]
-    p = lambda i, j: i*h+j
-    for i in range(w):
-        for j in range(h):
-            pts.extend((x[i, j], y[i, j], 0))
-            if i + 1 < w and j + 1 < h:
-                connectivity.append( (
-                        visit_writer.quad,
-                        p(i, j), p(i+1, j), p(i+1, j+1), p(i, j+1),
-                    ) )
-                zonal.append(C[i, j])
-            nodal.append(C[i, j])
-    return pts, connectivity, zonal, nodal
+from pcolor import pcolor_unstructured
 
 # Create a simple demo (circular) mesh
 r, phi = meshgrid(arange(0, 1, 0.1), arange(-3, 3, 0.1))
